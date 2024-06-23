aa32ebb6e1c6d9003e8ca037a73e81ea5feb0a3e
Yupindra Kumar (CW) <ykumar@synaptics.com>
2024-06-23 12:52:15 +0530

    Tkinter Calculator Application
    Added sub/div/mul operations as well

--- tkinter_programming/calculator_app.py
+++ tkinter_programming/calculator_app.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 root.title('Simple Calculator')
 
-entry = Entry(root, width=35, borderwidth=5)
+entry = Entry(root, width=40, borderwidth=5)
 entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 def clickMe(number):
@@ -12,16 +12,49 @@ def clickMe(number):
     entry.insert(0, str(prev) + str(number))
 
 def clickAdd():
-    global fNum
+    global fNum, operation
     firstNumber = entry.get()
     fNum = firstNumber
     entry.delete(0, END)
+    operation = "add"
 
+def clickSub():
+    global fNum, operation
+    firstNumber = entry.get()
+    fNum = firstNumber
+    entry.delete(0, END)
+    operation = "Sub"
+
+def clickMul():
+    global fNum, operation
+    firstNumber = entry.get()
+    fNum = firstNumber
+    entry.delete(0, END)
+    operation = "Mul"
+
+def clickDiv():
+    global fNum, operation
+    firstNumber = entry.get()
+    fNum = firstNumber
+    entry.delete(0, END)
+    operation = "Div"
 
 def clickEqual():
+    global operation
     secondNumber = entry.get()
     entry.delete(0, END)
-    result = int(fNum) + int(secondNumber)
+    try:
+        if operation == 'add':
+            result = int(fNum) + int(secondNumber)
+        elif operation == 'Sub':
+            result = int(fNum) - int(secondNumber)
+        elif operation == 'Mul':
+            result = int(fNum) * int(secondNumber)
+        elif operation == 'Div':
+            result = int(fNum) / int(secondNumber)
+    except:
+        print("Something broken. fix it!")
+
     entry.insert(0, str(result))
 
 def clickClear():
@@ -41,6 +74,10 @@ myButtonAdd = Button(root, text='+', padx=39, pady=20, command=clickAdd)
 myButtonEqual = Button(root, text='=', padx=91, pady=20, command=clickEqual)
 myButtonClear = Button(root, text='Clear', padx=79, pady=20, command=clickClear)
 
+myButtonSub = Button(root, text='-', padx=41, pady=20, command=clickSub)
+myButtonMul = Button(root, text='*', padx=40, pady=20, command=clickMul)
+myButtonDiv = Button(root, text='/', padx=41, pady=20, command=clickDiv)
+
 
 myButton1.grid(row=3, column=0)
 myButton2.grid(row=3, column=1)
@@ -57,6 +94,8 @@ myButtonAdd.grid(row=5, column=0)
 myButtonEqual.grid(row=5, column=1, columnspan=2)  
 myButtonClear.grid(row=4, column=1, columnspan=2)
 
-
+myButtonSub.grid(row=6, column=0)
+myButtonMul.grid(row=6, column=1)
+myButtonDiv.grid(row=6, column=2)
 
 root.mainloop()
